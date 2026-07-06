0dc5217155443c34b3f3702e9bae910f4b6d4e5e
[dyad] <ittranvandong@gmail.com>
2026-07-06 11:29:56 +0700

    Updated app/db/client.server.ts

--- app/db/client.server.ts
+++ app/db/client.server.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 import * as schema from "./schema/index";
 
 const pool = new pg.Pool({
-  connectionString: "",
+  connectionString: "postgresql://card_game_08hl_user:yPjctkhzrtzG6OjGtt49TbZ9pMCca68o@dpg-d927lj99rddc738744bg-a.singapore-postgres.render.com/card_game_08hl?uselibpqcompat=true&sslmode=require",
 });
 
 export const db = drizzle(pool, { schema });
