5e5e878c0ecd639ca1a6553453652c03c6856438
dongtran <dongtv2510@gmail.com>
2026-09-16 15:54:13 +0700

    ficx

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/80",
+        default: "bg-primary/50 text-primary-foreground hover:bg-primary",
         outline:
           "border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:bg-transparent dark:hover:bg-input/30",
         secondary:
