07f7d953bbc34b68bf9305210406d242102a0534
dongtran <dongtv2510@gmail.com>
2026-09-21 15:04:18 +0700

    fix font

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="font-sfu-freeway">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -64,7 +64,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto font-sfu-freeway">
+    <main className="pt-16 p-4 container mx-auto">
       <h1>{message}</h1>
       <p>{details}</p>
       {stack && (
